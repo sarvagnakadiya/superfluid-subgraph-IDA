65f2773a70bc9cd62ca7ea1a5436c3d3d2553e62
sarvagna <sarvagnakadiya1@gmail.com>
2023-01-19 11:27:08 +0530

    query changes

--- subgraph.sql
+++ subgraph.sql
@@ -1,16 +1,25 @@
 query MyQuery {
   indexes(where: {publisher_: {id: "0xcc920c851327af767b4bf770e3b2c2ea50b90fde"}}) {
     publisher {
-      id
+     id
       publishedIndexes {
         id
+        indexValue
+        totalUnits
+        totalUnitsPending
+        totalUnitsApproved
+        subscriptions {
+          id
+          approved
+          units
+        }
       }
     }
-    subscriptions {
-      approved
-      subscriber {
-        id
-      }
-    }
+    # subscriptions {
+    #   approved
+    #   subscriber {
+    #     id
+    #   }
+    # }
   }
 }
